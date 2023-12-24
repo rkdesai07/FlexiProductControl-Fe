c27689460aaf585592db1f9b08cd3ef30fe026e2
rkdesai07 <desairajeshrk36@gmail.com>
2023-12-24 18:13:27 +0530

    Create initial project

---
+++ app/(marketing)/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+// import ModeToggle from  "@/components/ThemeMode/ModeToggle"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button>dd</Button >
+      {/* <ModeToggle /> */}
+    </main>
+  )
+}
